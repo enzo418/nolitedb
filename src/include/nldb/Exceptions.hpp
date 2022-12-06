170687d6939ad5573b164d94a27c26d5e5fa5546
enzo418 <c.enzoaguirre@gmail.com>
2022-12-05 22:24:46 -0300

    move exceptions to namespace

--- src/include/nldb/Exceptions.hpp
+++ src/include/nldb/Exceptions.hpp
@@ -2,37 +2,40 @@
 
 #include <stdexcept>
 
-typedef const std::string& str;
+namespace nldb {
+    typedef const std::string& str;
 
-class CollectionNotFound : public std::runtime_error {
-   public:
-    CollectionNotFound(str extra = "")
-        : std::runtime_error("Collection not found " + extra) {}
-};
+    class CollectionNotFound : public std::runtime_error {
+       public:
+        CollectionNotFound(str extra = "")
+            : std::runtime_error("Collection not found " + extra) {}
+    };
 
-class PropertyNotFound : public std::runtime_error {
-   public:
-    PropertyNotFound(str extra = "")
-        : std::runtime_error("Property not found " + extra) {}
-};
+    class PropertyNotFound : public std::runtime_error {
+       public:
+        PropertyNotFound(str extra = "")
+            : std::runtime_error("Property not found " + extra) {}
+    };
 
-class DocumentNotFound : public std::runtime_error {
-   public:
-    DocumentNotFound(str extra = "")
-        : std::runtime_error("Document not found " + extra) {}
-};
+    class DocumentNotFound : public std::runtime_error {
+       public:
+        DocumentNotFound(str extra = "")
+            : std::runtime_error("Document not found " + extra) {}
+    };
 
-class WrongPropertyType : public std::runtime_error {
-   public:
-    WrongPropertyType(str pName = "", str pExpected = "", str pActual = "")
-        : std::runtime_error("Wrong property type for " + pName +
-                             ", expected " + pExpected + " actual " + pActual),
-          propertyName(pName),
-          expected(pExpected),
-          actual(pActual) {}
+    class WrongPropertyType : public std::runtime_error {
+       public:
+        WrongPropertyType(str pName = "", str pExpected = "", str pActual = "")
+            : std::runtime_error("Wrong property type for " + pName +
+                                 ", expected " + pExpected + " actual " +
+                                 pActual),
+              propertyName(pName),
+              expected(pExpected),
+              actual(pActual) {}
 
-   public:
-    std::string propertyName;
-    std::string expected;
-    std::string actual;
-};
+       public:
+        std::string propertyName;
+        std::string expected;
+        std::string actual;
+    };
+}  // namespace nldb
